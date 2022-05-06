3732af1ebc0eab08fbc4627921cd6f40506f0291
Raphael ASTIER <r.astier.dev@gmail.com>
2022-05-06 18:27:39 -0400

    Setup users to use flat files

--- config/auth.php
+++ config/auth.php
@@ -61,8 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'driver' => 'statamic',
         ],
 
         // 'users' => [
