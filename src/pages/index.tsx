74903b0e5998c74745944f58ebd1209c1dc3f83c
Raphael ASTIER <r.astier.dev@gmail.com>
2022-05-11 16:01:36 -0400

    Add i18n test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import * as React from "react"
-import { graphql, PageProps } from 'gatsby';``
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { graphql, PageProps } from 'gatsby';
 
 // markup
 const IndexPage = ({data}: PageProps) => {
+    const {t} = useTranslation();
+
     return (
         <div>
-            <p className="text-3xl font-display">Construisons Montréal</p>
+            <p className="text-3xl font-display">{t('site_title')}</p>
             <pre>
                 {JSON.stringify(data)}
             </pre>
