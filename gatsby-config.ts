f52542587d69962a6b4b6afc268e9aefb25fc548
Raphael ASTIER <r.astier.dev@gmail.com>
2022-08-27 11:17:52 -0400

    Fix analytics tracking

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,10 +9,27 @@ const config: GatsbyConfig = {
     siteUrl: `https://www.construisonsmtl.ca`
   },
   plugins: ["gatsby-plugin-postcss", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-XL2QVGXY9B"
-    }
+    resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-XL2QVGXY9B", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: []
+        },
+      }
   }, {
     resolve: `gatsby-plugin-manifest`,
     options: {
