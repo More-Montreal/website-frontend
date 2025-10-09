b2a6be19761092f9549e52a87f7557687e45b579
abclive <r.astier.dev@gmail.com>
2025-10-08 21:27:45 -0400

    Add parties logos

--- src/pages/cities-policies.tsx
+++ src/pages/cities-policies.tsx
@@ -1,6 +1,6 @@
 import { graphql, Link, PageProps } from "gatsby";
 import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { useTranslation } from "@herob191/gatsby-plugin-react-i18next";
 import Button, { ButtonType } from "../components/button";
 import Footer from "../components/footer";
@@ -78,8 +78,8 @@ const PoliciesPage = ({ data }: PageProps<CityPoliciesPageData>) => {
     const renderPoliticalPartiesHeader = () => (
         <div className={`hidden xl:visible xl:grid ${gridCols[cityPoliticalParties.length]} gap-4 mt-4 sticky top-4`}>
             {cityPoliticalParties.map((party, pi) => (
-                <div key={party} className={`inline-flex w-fit px-4 py-2 text-lg font-medium border backdrop-blur-md rounded-full ${partyColor(pi)}`}>
-                    {party}
+                <div key={party} className={`inline-flex w-fit px-4 py-2 self-start text-lg font-medium border backdrop-blur-md rounded-full bg-gray-50/50`}>
+                    {partyLogos[party] ?? <p className="text-gray-600">{party}</p>}
                 </div>
             ))}
         </div>
@@ -93,15 +93,13 @@ const PoliciesPage = ({ data }: PageProps<CityPoliciesPageData>) => {
         5: "grid-cols-1 xl:grid-cols-5",
     };
 
-    const partyColors: Record<number, string> = {
-        0: "bg-blue-50/50 text-blue-800 border-blue-100",
-        1: "bg-indigo-50/50 text-indigo-800 border-indigo-100",
-        2: "bg-purple-50/50 text-purple-800 border-purple-100",
-        3: "bg-pink-50/50 text-pink-800 border-pink-100",
-        4: "bg-red-50/50 text-red-800 border-red-100",
+    const partyLogos: Record<string, ReactNode> = {
+        "Projet Montréal": <StaticImage src="../images/parties/projet-montreal.png" alt="Projet Montreal" height={40} />,
+        "Ensemble Montréal": <StaticImage src="../images/parties/ensemble-montreal.png" alt="Ensemble Montreal" height={40} />,
+        "Futur Montréal": <StaticImage src="../images/parties/futur-montreal.png" alt="Futur Montreal" height={40} />,
+        "Transition Montréal": <StaticImage src="../images/parties/transition-montreal.webp" alt="Transition Montreal" height={40} />,
+        "Coalition Longueuil": <StaticImage src="../images/parties/coalition-longueuil.png" alt="Coalition Longueuil" height={40} />,
     };
-
-    const partyColor = (index: number) => partyColors[index % Object.keys(partyColors).length];
     
     return (
         <div>
@@ -135,8 +133,8 @@ const PoliciesPage = ({ data }: PageProps<CityPoliciesPageData>) => {
                         </div>
                     </div>
                     <div className="flex flex-col items-center justify-center flex-1 gap-4 px-4 py-10 lg:px-32 lg:py-24 text-center">
-                        <h1 className="text-2xl font-bold text-white lg:text-4xl font-display">{content.heroTitle}</h1>
-                        <p className="text-lg font-medium text-white opacity-80">{content.heroDescription}</p>
+                        <h1 className="text-2xl font-bold text-white lg:text-4xl font-display text-balance">{content.heroTitle}</h1>
+                        <p className="text-lg font-medium text-white opacity-80 text-balance">{content.heroDescription}</p>
                     </div>
                 </div>
             </div>
@@ -154,8 +152,8 @@ const PoliciesPage = ({ data }: PageProps<CityPoliciesPageData>) => {
                                 <div className={`grid ${gridCols[cityPoliticalParties.length]} gap-10 xl:gap-4`}>
                                     {cityPoliticalParties.map((party, pi) => (
                                         <div className="flex flex-col gap-4 xl:gap-0">
-                                            <div key={party} className={`xl:hidden inline-flex w-fit px-4 py-2 font-medium border backdrop-blur-md rounded-full ${partyColor(pi)}`}>
-                                                {party}
+                                            <div key={party} className={`xl:hidden inline-flex w-fit px-4 py-2 font-medium border backdrop-blur rounded-full sticky top-4`}>
+                                                {partyLogos[party] ?? <p className="text-gray-600">{party}</p>}
                                             </div>
                                             {getAnswerForQuestionAndParty(q, party).map((a, ai) => (
                                                 <div key={ai} className={`p-4 ${pi % 2 === 0 ? "bg-white" : "bg-gray-50"} rounded-lg`}>
